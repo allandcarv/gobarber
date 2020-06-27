8431c38c54be27cbdbffcf5a6b984c30bd13e62c
Allan Carvalho <allandcarv@gmail.com>
2020-06-27 16:05:51 +0100

    Models Relationship

--- src/database/migrations/1593200795305-CreateAppointments.ts
+++ src/database/migrations/1593200795305-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1593200795305
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',

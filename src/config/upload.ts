bb33bba7a7da23e5e69a6b071fdb6967adbe73ad
Allan Carvalho <allandcarv@gmail.com>
2020-07-18 13:19:23 +0100

    Adding Cors

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -8,7 +8,7 @@ export default {
     directory: tmpFolder,
 
     storage: multer.diskStorage({
-        destination: resolve(__dirname, '..', '..', 'tmp'),
+        destination: tmpFolder,
         filename(request, file, callback) {
             const fileHash = crypto.randomBytes(10).toString('hex');
             const fileName = `${fileHash}-${file.originalname}`;

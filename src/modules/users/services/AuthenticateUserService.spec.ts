22e191ae100a4451805b036fa523491bd826332f
Allan Carvalho <allandcarv@gmail.com>
2020-08-22 20:38:46 +0100

    Test Authentication

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -25,4 +25,33 @@ describe('AuthenticateUserService', () => {
         expect(response).toHaveProperty('token');
         expect(response.user).toEqual(user);
     })
+
+    it('should not be able to authenticate with unknown user', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeBCryptHashProvider();
+        const authenticateUsersService = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+
+        expect(authenticateUsersService.execute({
+            email: 'user@example.com',
+            password: '12345'
+        })).rejects.toBeInstanceOf(AppError);
+    })
+
+    it('should not be able to authenticate with wrong password', async () => {
+        const fakeUsersRepository = new FakeUsersRepository();
+        const fakeHashProvider = new FakeBCryptHashProvider();
+        const authenticateUsersService = new AuthenticateUserService(fakeUsersRepository, fakeHashProvider);
+        const createUsersService = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+
+        await createUsersService.execute({
+            name: 'User Example',
+            email: 'user@example.com',
+            password: '12345'
+        })
+
+        expect(authenticateUsersService.execute({
+            email: 'user@example.com',
+            password: 'wrong-password'
+        })).rejects.toBeInstanceOf(AppError);
+    })
 })
